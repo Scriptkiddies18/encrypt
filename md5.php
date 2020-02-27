6b8689920de2c282f0b10f12056a1019c80a1f30
Scriptkiddies18 <43102755+Scriptkiddies18@users.noreply.github.com>
2020-02-28 02:35:04 +0700

    Update md5.php

--- md5.php
+++ md5.php
@@ -2,7 +2,7 @@
 
 echo "Masukin Nama Lo : ";
 $name = trim(fgets(STDIN));
-if($name == 'rafli'){
+if($name == 'rapli'){
 	echo "Nama Yang Bagus! \n";
 }if($name == 'Rafli'){
 	echo "Nama Yang Bagus! \n";
@@ -40,14 +40,14 @@ echo "\n";
 echo "Pilih [1] or [2] : ";
 $pilihan = trim(fgets(STDIN));
 if ($pilihan == '1') {
-	echo "Paste Disini : ";
+	echo "Tulis Disini : ";
 	$hash = trim(fgets(STDIN));
-	echo "Hasilnya : ".md5($hash)."\n";
+	echo "Hasilnya : [ ".md5($hash)." ]\n";
 }if ($pilihan == '2') {
 	echo "Paste Text Encryptnya : ";
 	$pass = trim(fgets(STDIN));
 	$hasil = file_get_contents("http://www.nitrxgen.net/md5db/".$pass);
-	echo "Hasilnya : ".$hasil."\n";
+	echo "Hasilnya : [ ".$hasil." ]\n";
 }
 
 ?>
